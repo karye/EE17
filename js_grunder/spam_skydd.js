bcb3a82ace1c1cc14f852d34e69b9bdb8d21428b
Karye <karye@users.noreply.github.com>
2018-10-18 15:18:37 +0200

    Labb spamskydd

---
+++ js_grunder/spam_skydd.js
@@ -0,0 +1,38 @@
+/* 
+ * Skapa ett spamskydd
+ * Två slumptal presenteras
+ * Användaren räknar ut summan
+ */
+
+/* Kör koden när webbsidan laddtas klart */
+window.onload = start;
+
+function start() {
+
+    /* Skapar globala variabler */
+
+
+    /* Slumpa fram två tal */
+
+
+    /* Skriv ut frågan */
+    /* 1. Ta tag i target som vi vill jobba med */
+    /* 2. Skriv ut, tex "Vad är 2+3?"; */
+
+    /* Aktivera knappen, dvs lyssna på knappen */
+    /* 1. Ta tag i knappen */
+    /* 2. Lyssna på knappen */
+
+    /* En funktion */
+        /* Läs in det som användaren skriver i textrutan */
+        /* 1. Ta tag i textrutan */
+        /* 2. Läs av innehållet */
+
+
+        /* Jämför summa med svaret */
+
+            /* Skriv ut svar: "Rätt svar, du är en människa!" */
+
+            /* Skriv ut svar: "Fel svar, du en robot eller en människa som räknar fel!" */
+        
+}
--- js_larare/spam_skydd.js
+++ js_larare/spam_skydd.js
@@ -1,15 +1,16 @@
 /* 
-* Skapa ett spamskydd
-* Två slumptal presenteras
-* Användaren räknar ut summan
-*/
+ * Skapa ett spamskydd
+ * Två slumptal presenteras
+ * Användaren räknar ut summan
+ */
+
 /* Kör koden när webbsidan laddtas klart */
-window.onload = init;
+window.onload = start;
 
-/* Skapar globala variabler */
-var tal1, tal2;
+function start() {
 
-function init() {
+    /* Skapar globala variabler */
+    var tal1, tal2;
 
     /* Slumpa fram två tal */
     tal1 = Math.ceil(Math.random() * 5);
@@ -17,35 +18,38 @@ function init() {
 
     /* Skriv ut frågan */
     /* 1. Ta tag i elementet som vi vill jobba med */
-    const eleFragan = document.querySelector('#fragan');
+    const elementFragan = document.querySelector('#fragan');
     /* 2. Skriv ut */
-    /* eleFragan.textContent = "Vad är 2+3?"; */
-    eleFragan.textContent = "Vad är " + tal1 + " + " + tal2 + "?";
+    /* eleFragan.textContent = Tex "Vad är 2+3?"; */
+    elementFragan.textContent = "Vad är " + tal1 + " + " + tal2 + "?";
 
     /* Aktivera knappen, dvs lyssna på knappen */
     /* 1. Ta tag i knappen */
-    const eleKnappen = document.querySelector("#knappen");
+    const elementKnappen = document.querySelector("#knappen");
     /* 2. Lyssna på knappen */
-    eleKnappen.addEventListener("click", spamSkydd);
-}
+    elementKnappen.addEventListener("click", spamSkydd);
 
-function spamSkydd() {
-    /* Testa att det funka att klicka på knappen */
-    console.log("spamSkydd anropat!");
-    
-    /* Läs in det som användaren skriver i textrutan summa */
-    /* 1. Ta tag i textrutan */
-    const eleSumman = document.querySelector("#summan");
-    /* 2. Läs av innehållet */
-    var summa = eleSumman.value;
-    console.log(summa);
-    
-    /* Jämför summa med svaret */
-    if (summa == (tal1 + tal2)) {
-        console.log("Rätt!");
-        
-    } else {
-        console.log("Fel!");
-        
+    function spamSkydd() {
+        /* Testa att det funka att klicka på knappen */
+        console.log("spamSkydd anropat!");
+
+        /* Läs in det som användaren skriver i textrutan summa */
+        /* 1. Ta tag i textrutan */
+        const elementSumman = document.querySelector("#summan");
+        /* 2. Läs av innehållet */
+        var summa = elementSumman.value;
+        console.log(summa);
+
+        /* Jämför summa med svaret */
+        if (summa == (tal1 + tal2)) {
+            console.log("Rätt!");
+
+            /* Skriv ut svar: "Rätt svar, du är en människa!" */
+
+        } else {
+            console.log("Fel!");
+
+            /* Skriv ut svar: "Fel svar, du en robot eller en människa som räknar fel!" */
+        }
     }
 }
