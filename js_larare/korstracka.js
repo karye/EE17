6894187279cd252e7d428fb93b03384bac19c298
Karye <karye@users.noreply.github.com>
2018-10-19 10:51:57 +0200

    Förberedelser labb Körsträcka

---
+++ js_larare/korstracka.js
@@ -0,0 +1,34 @@
+/* 
+ * Räknar ut en körsträckan för en bil.
+ * Mata in hastighet och tid.
+ * Få avtsånd som svar.
+ */
+
+/* Kör koden när webbsidan laddats klart */
+window.onload = start;
+
+function start() {
+
+    /* Välj ut alla elementen att jobba med: target */
+    ???
+    ???
+    ???
+    ???
+
+    /* Lyssna på knappen */
+    ???
+
+    /* Räkna ut avståndet */
+    function raknaUt() {
+        
+        /* Läs av hastigheten och tiden */
+        ???
+        ???
+
+        /* Räkna ut avståndet = hastigheten * tid */
+        var avstand = ???;
+
+        /* Skriv ut resultatet tex 'Körsträckan blir 57 km.' */
+        ???
+    }
+}
