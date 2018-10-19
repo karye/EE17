17859054b9ad428f50f58b384309c2042f9024db
Karye <karye@users.noreply.github.com>
2018-10-19 13:27:21 +0200

    Förberedelser labb CSS filter

---
+++ js_grunder/filter.js
@@ -0,0 +1,28 @@
+/* 
+* Testar tre oilka filter på ett foto.
+* Foto kommer från http://unsplash.com
+*/
+
+/* När webbsidan laddtas klart kör start() */
+window.onload = start;
+
+/* Start-delen */
+function start() {
+    
+    /* Välj ut alla elementen att jobba med: target */
+
+
+    /* Lyssna på knapparna */
+
+
+    /* Gör fotot suddigt */
+    function suddigt() {
+
+    }
+
+    /* Gör fotot gråskala */
+
+
+    /* Gör fotot sepia-färgat */
+
+}
