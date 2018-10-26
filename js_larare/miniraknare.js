6ab10c870864209a9da7b9ad1ba50cda9ba750d0
Karye <karye@users.noreply.github.com>
2018-10-26 11:03:34 +0200

    Förberedelser för labb miniräknare

--- js_larare/miniraknare.js
+++ js_larare/miniraknare.js
@@ -4,7 +4,30 @@ window.onload = start;
 function start() {
 
     /* Elementen vi jobbar med */
-    const elementTal1 = document.querySelector('#tal1');
+    const ...
+    const ...
+    const ...
+    const ...
+    
+    /* Lyssna på knapp Plus */
+    ...addEventListener(...)
 
     /* Addera tal1 + tal2 = resultat */
+    /* Läs av värdet av inputruta 1 */
+    var ... = ....value;
+    /* Läs av värdet av inputruta 2 */
+    var ... = ....value;
+
+    /* Summera */
+    var resultat = ... + ...
+
+    /* Skriv resultatet i inputruta 3 */
+    ....value = ...
+
+    /* Knappen - (minus) */
+
+    /* Knappen * (gånger) */
+
+    /* Knappen / (dividera) */
+    
 }
