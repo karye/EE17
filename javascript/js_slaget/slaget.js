5921e74bff64f1f84daf48d3431334ad3f228bde
Karye <karye@users.noreply.github.com>
2018-12-07 09:32:51 +0100

    Tränar på att använda ett kartramverk (library)

---
+++ javascript/js_slaget/slaget.js
@@ -0,0 +1,9 @@
+/* 
+I den här uppgiften ska du skapa en slagsmålssimulator med två inblandade slagskämpar.
+Simulatorn ska hålla reda på slagskämparnas namn och hp.
+Simulatorn körs så länge båda slagskämparna har hp kvar.
+Varje gång loopen körs – varje “runda” – så ska slagskämpe A slå B, och B ska slå A.
+Ett slag innebär att motståndaren blir av med en slumpmässig mängd hp.
+När striden är slut, presentera vinnaren – eller om båda slagskämparna kom under 0 samtidigt, förklara att det blev oavgjort.
+VIKTIGT: Om du får mycket hjälp av en kamrat, skriv det i dina kommentarer!
+*/
