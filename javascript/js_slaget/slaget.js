f8491b683f414abe891559ba85c0c8457ffb666a
Karim Ryde <karim.ryde@learnet.se>
2019-01-14 15:22:47 +0100

    Nästan klar

--- javascript/js_slaget/slaget.js
+++ javascript/js_slaget/slaget.js
@@ -12,6 +12,46 @@ window.onload = start;
 
 /* Koden som körs från start */
 function start() {
-    
-    
+    /* Alla element vi behöver komma åt */
+    const eInput1 = document.querySelector("#spelare1");
+    const eInput2 = document.querySelector("#spelare2");
+    const eP = document.querySelector("#resultat");
+    const eButton = document.querySelector("#knapp");
+
+    /* Ange hp vid start */
+    let hp1 = 100;
+    let hp2 = 100;
+
+    eButton.addEventListener("click", runda);
+    function runda() {
+        /* Läs in namnen på spelarna */
+        let spelare1 = eInput1.value;
+        let spelare2 = eInput2.value;
+        console.log("spelare1 =" + spelare1);
+        console.log("spelare2 =" + spelare2);
+
+        /* Slumpa ett tal mellan 1 och 10 */
+        let slumptal1 = Math.ceil(Math.random() * 10 + 1);
+        let slumptal2 = Math.ceil(Math.random() * 10 + 1);
+
+        /* Drav slumptal från hp */
+        hp1 = hp1 - slumptal1;
+        hp2 = hp2 - slumptal2;
+
+        /* Skriv resultatet av slaget */
+        eP.innerHTML += spelare1 + " förlorar med " + slumptal1 + " har nu " + hp1 + ", " + spelare2 + " förlorar med " + slumptal2 + " har nu " + " har " + hp2 + "<br>";
+
+        /* Vem har vunnit? */
+        if (hp1 <= 0 && hp2 > 0) {
+            eP.innerHTML += spelare2 + " har vunnit!";
+        } else {
+            if (hp2 <= 0 && hp1 > 0) {
+                eP.innerHTML += spelare1 + " har vunnit!"; 
+            } else {
+                if (hp1 == 0 && hp2 == 0) {
+                    eP.innerHTML += "Matchen är oavgjord!";
+                }
+            }
+        }
+    }
 }
