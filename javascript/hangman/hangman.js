9a789e36e9271834bd952da6cd1b9bb404ca526e
Karim Ryde <karim.ryde@learnet.se>
2019-05-20 09:34:31 +0200

    Rita klart, if-satser för att visa steg-för-steg

--- javascript/hangman/hangman.js
+++ javascript/hangman/hangman.js
@@ -3,35 +3,85 @@ window.onload = start;
 function start() {
     var canvas = document.querySelector("#myCanvas");
     var ctx = canvas.getContext("2d");
+    var raknare = 0;
 
-    /* Kroppen */
-    ctx.moveTo(250, 100);
-    ctx.lineTo(250, 250);
-    ctx.stroke();
+    /* Element vi jobbar med */
+    const eKnapp = document.querySelector("button");
 
-    /* Höger arm */
-    ctx.moveTo(250, 150);
-    ctx.lineTo(300, 200);
-    ctx.stroke();
+    /* Lyssna på knappen */
+    eKnapp.addEventListener("click", visaHangman);
 
-    /* Vänster arm */
-    ctx.moveTo(250, 150);
-    ctx.lineTo(200, 200);
-    ctx.stroke();
+    function visaHangman() {
+        raknare++;
+        console.log(raknare);
 
-    /* Höger ben */
-    ctx.beginPath();
-    ctx.moveTo(250, 250);
-    ctx.lineTo(300, 350);
-    ctx.stroke();
+        if (raknare == 1) {
+            /* Jorden */
+            ctx.arc(300, 800, 400, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
 
-    /* Vänster ben */
-    ctx.moveTo(250, 250);
-    ctx.lineTo(200, 350);
-    ctx.stroke();
+        if (raknare == 2) {
+            /* Galgen 1:3 */
+            ctx.moveTo(200, 420);
+            ctx.lineTo(200, 30);
+            ctx.stroke();
+        }
 
-    /* Huvudet */
-    ctx.beginPath();
-    ctx.arc(250, 70, 30, 0, 2 * Math.PI);
-    ctx.stroke();
+        if (raknare == 3) {
+            /* Galgen 2:3 */
+            ctx.moveTo(200, 30);
+            ctx.lineTo(450, 30);
+            ctx.stroke();
+        }
+
+        if (raknare == 4) {
+            /* Galgen 3:3 */
+            ctx.moveTo(400, 30);
+            ctx.lineTo(400, 70);
+            ctx.stroke();
+        }
+
+        if (raknare == 5) {
+            /* Huvudet */
+            ctx.beginPath();
+            ctx.arc(400, 100, 30, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+
+        if (raknare == 6) {
+            /* Kroppen */
+            ctx.moveTo(400, 130);
+            ctx.lineTo(400, 250);
+            ctx.stroke();
+        }
+
+        if (raknare == 7) {
+            /* Höger arm */
+            ctx.moveTo(400, 150);
+            ctx.lineTo(450, 200);
+            ctx.stroke();
+        }
+
+        if (raknare == 8) {
+            /* Vänster arm */
+            ctx.moveTo(400, 150);
+            ctx.lineTo(350, 200);
+            ctx.stroke();
+        }
+
+        if (raknare == 9) {
+            /* Höger ben */
+            ctx.moveTo(400, 250);
+            ctx.lineTo(450, 350);
+            ctx.stroke();
+        }
+
+        if (raknare == 10) {
+            /* Vänster ben */
+            ctx.moveTo(400, 250);
+            ctx.lineTo(350, 350);
+            ctx.stroke();
+        }
+    }
 }
